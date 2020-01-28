3e347c267dc6ef431f8f502d486b975130b703f3
saimore027 <53985214+saimore027@users.noreply.github.com>
2020-01-28 19:02:20 +0530

    TurtleProgram

---
+++ TurtleProgram.py
@@ -0,0 +1,34 @@
+import turtle
+
+sai=turtle.Turtle()
+turtle.speed(30)
+def square():
+    sai.forward(100)
+    sai.right(90)
+    sai.forward(100)
+    sai.right(90)
+    sai.forward(100)
+    sai.right(90)
+    sai.forward(100)
+    
+    
+
+for i in range(10):
+    square()
+print(float(2))
+string="Hello"
+print(string[0])
+print(string.lower())
+print(string.replace("Hello","Pooja ki maa Ka"))
+splitting=string.split(" ",4)
+print(splitting)
+
+name=["John" ,"sai" ,"shivam" ,"aniket"]
+print(name)
+name.append("gary")
+print(name)
+
+name.insert(0,"gary")
+print(name)
+
+    
